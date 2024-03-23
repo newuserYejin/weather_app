262363d6c940e627dbec9545651ddb9c5d2e305e
jin <jin200126@gmail.com>
2024-03-23 23:13:24 +0900

    update

--- src/component/WeatherBox.js
+++ src/component/WeatherBox.js
@@ -4,7 +4,7 @@ const WeatherBox = ({ weather }) => {
     return (
         <div className='weatherbox'>
             <div>Location: {weather?.name}</div>
-            <div>temp: {weather?.main.temp} / {((weather?.main.temp * 9 / 5) + 32).toFixed(2)}</div>
+            <div>temp: {weather?.main.temp}&deg;C / {((weather?.main.temp * 9 / 5) + 32).toFixed(2)}&deg;F</div>
             <div>{weather?.weather[0].description}</div>
         </div >
     )
